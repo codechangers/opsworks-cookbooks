90c1f46be77d907fecbb297a283bcb5fa0c4bae8
Alejandro Carmona <acarmona22@gmail.com>
2016-05-05 17:33:21 -0500

    Test wkhtmltopdf recipee

--- wkhtmltopdf/recipes/binary.rb
+++ wkhtmltopdf/recipes/binary.rb
@@ -9,24 +9,23 @@ remote_file download_dest do
 end
 
 execute 'extract_wkhtmltopdf' do
-  command "tar xf #{cache_dir}/#{download_dest}"
+  command "tar xf #{cache_dir}#{download_dest}"
   creates File.join(cache_dir, 'wkhtmltox')
 end
 
 execute 'install_wkhtmltoimage' do
-  command "cp #{cache_dir}/wkhtmltox/bin/wkhtmltoimage #{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
+  command "cp #{cache_dir}wkhtmltox/bin/wkhtmltoimage #{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
   creates "#{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
 end
 
 execute 'install_wkhtmltopdf' do
-  command "cp #{cache_dir}/wkhtmltox/bin/wkhtmltopdf #{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
+  command "cp #{cache_dir}wkhtmltox/bin/wkhtmltopdf #{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
   creates "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
 end
 
 unless node['wkhtmltopdf']['lib_dir'].empty?
   execute 'install_wkhtmltox_so' do
-    cwd cache_dir
-    command "cp wkhtmltox/lib/libwkhtmltox.so.#{node['wkhtmltopdf']['version']} #{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
+    command "cp #{cache_dir}wkhtmltox/lib/libwkhtmltox.so.#{node['wkhtmltopdf']['version']} #{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
     creates "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
   end
 
