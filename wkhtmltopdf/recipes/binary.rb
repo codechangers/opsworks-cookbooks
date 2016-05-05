86901a8bf56d4968ba1e05cbc967e9213f173850
Alejandro Carmona <acarmona22@gmail.com>
2016-05-05 17:17:41 -0500

    Test wkhtmltopdf recipee

--- wkhtmltopdf/recipes/binary.rb
+++ wkhtmltopdf/recipes/binary.rb
@@ -2,18 +2,6 @@ cache_dir = Chef::Config[:file_cache_path]
 download_dest = File.join(cache_dir, node['wkhtmltopdf']['archive'])
 wkhtmltopdf_version = Chef::Version.new(node['wkhtmltopdf']['version'])
 
-remote_file download_dest do
-  source node['wkhtmltopdf']['mirror_url']
-  mode '0644'
-  action :create_if_missing
-end
-
-execute 'extract_wkhtmltopdf' do
-  cwd cache_dir
-  command "tar xf #{download_dest}"
-  creates File.join(cache_dir, 'wkhtmltox')
-end
-
 execute 'install_wkhtmltoimage' do
   cwd cache_dir
   command "cp wkhtmltox/bin/wkhtmltoimage #{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
