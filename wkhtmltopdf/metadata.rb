926bd24634237dabea8fc3a1122f0dc80d2c4afa
Alejandro Carmona <acarmona22@gmail.com>
2016-05-05 15:20:46 -0500

    Changed metadata of wkhtmltopdf to make it work

--- wkhtmltopdf/metadata.rb
+++ wkhtmltopdf/metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Brian Flad'
 maintainer_email 'bflad417@gmail.com'
 license 'Apache 2.0'
 description 'Installs wkhtmltoimage and wkhtmltopdf'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 
 recipe 'wkhtmltopdf', 'Installs wkhtmltoimage and wkhtmltopdf'
